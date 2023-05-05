08dfb2cffc7e4f181b230ca84345fc79355b9134
Rafael Licett <Rafael.licett@mentum.group>
2023-05-05 17:51:15 +0000

    redirect to login session guest

--- qp_phonix_front/qp_phonix_front/services/redirect_after_login.py
+++ qp_phonix_front/qp_phonix_front/services/redirect_after_login.py
@@ -1,5 +1,10 @@
+import frappe
 def handler(user = None):
 
+    if frappe.session.user == "Guest":
+    
+        return "/login"
+    
     if user != "Administrator":
         
         return "/order/item_formulary"
