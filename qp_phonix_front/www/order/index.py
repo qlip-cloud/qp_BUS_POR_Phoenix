9965736fd347a4a7d458b6f89b63ef94decde2de
Rafael Licett <rafael.licett@mentum.group>
2024-04-12 15:38:55 +0000

    add export button

--- qp_phonix_front/www/order/index.py
+++ qp_phonix_front/www/order/index.py
@@ -3,9 +3,12 @@ import frappe
 from qp_phonix_front.qp_phonix_front.validations.utils import is_guest
 from qp_phonix_front.qp_phonix_front.uses_cases.sales_order.sales_order import sales_order_list
 from qp_phonix_front.qp_phonix_front.services.try_catch import handler as try_catch
+from frappe.utils.xlsxutils import make_xlsx
+
+from datetime import datetime
 
 def get_context(context):
-
+    
     frappe.clear_cache()
         
     frappe.website.render.clear_cache()
@@ -19,3 +22,22 @@ def get_context(context):
         context.order_list = sales_order_list()
 
     try_catch(callback, context)
+
+
+@frappe.whitelist()
+def export():
+    now = datetime.now()
+
+    filename = f"Confirmadas {now.strftime('%d%m%Y%H%M%S')}"
+
+    content = [['Qlip ID', 'GP ID', 'Fecha de entrega']]
+
+    rows = frappe.get_list("Sales Order", filters = {"status": "To Deliver and Bill" }, fields = ["name", "qp_phonix_reference", "delivery_date"], as_list = True)
+    
+    content += list(map(lambda row: list(row),rows))
+   
+    xlsx_file = make_xlsx(content, filename)
+    # write out response as a xlsx type
+    frappe.response['filename'] = filename + '.xlsx'
+    frappe.response['filecontent'] = xlsx_file.getvalue()
+    frappe.response['type'] = 'binary'
