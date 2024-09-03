90ed669f54092eb58e9d512165127ac0f596cc7a
Rafael Licett <rafael.licettt@gmail.com>
2024-09-03 20:41:42 +0000

    [refactor] filter itemformulary

--- qp_phonix_front/resources/render/item_formulary/item_formulary_render.py
+++ qp_phonix_front/resources/render/item_formulary/item_formulary_render.py
@@ -16,11 +16,15 @@ def paginator(order_id = None, item_group = None, item_Categoria = None, item_Su
 
     error_msg = "Error render item formulary paginator"
     
+    has_filter_text = False
+    
     if filter_text:
             
             filter_text = filter_text.split(" ")
             
             filter_text = __get_filter_text(item_code_list, filter_text)
+            
+            has_filter_text = True
             
     if item_code_list:
         
@@ -28,31 +32,33 @@ def paginator(order_id = None, item_group = None, item_Categoria = None, item_Su
         
     def callback():
         
+        rows = []
         
+        if not has_filter_text or (has_filter_text and filter_text):
         
-        paginator_item = paginator_item_list(item_group, item_Categoria, item_SubCategoria, item_code_list, letter_filter, filter_text, idlevel = idlevel, 
-                                        has_inventary = json.loads(has_inventary), with_list_price = 'true' in [with_list_price], 
-                                        has_auto_coupon= json.loads(has_auto_coupon))
-        
-        
-        
-        
-        item_list = __get_item_list(filter_text, paginator_item)
+            paginator_item = paginator_item_list(item_group, item_Categoria, item_SubCategoria, item_code_list, letter_filter, filter_text, idlevel = idlevel, 
+                                            has_inventary = 'true' in [has_inventary], with_list_price = 'true' in [with_list_price], 
+                                            has_auto_coupon= 'true' in [has_auto_coupon])
+            
+            
+            
+            
+            item_list = __get_item_list(filter_text, paginator_item)
 
-        list(map(lambda x: x.update({"initial": x.item_name[0].upper()}), item_list))
+            list(map(lambda x: x.update({"initial": x.item_name[0].upper()}), item_list))
 
-        if order_id:
+            if order_id:
 
-            order_response = get_sales_order(order_id)
+                order_response = get_sales_order(order_id)
 
-            items_select = order_response.get("items")
+                items_select = order_response.get("items")
 
-            add_qty_item_list(item_list, items_select)
+                add_qty_item_list(item_list, items_select)
 
-        
-        permission = get_permission()
+            
+            permission = get_permission()
 
-        rows = frappe.render_template("templates/item_formulary/row.html", {"item_list" : item_list, "disabled_off": True, "permission": permission})
+            rows = frappe.render_template("templates/item_formulary/row.html", {"item_list" : item_list, "disabled_off": True, "permission": permission})
 
         return {
             "status": 200,
@@ -105,19 +111,7 @@ def __get_filter_text(item_code,filter_text):
 def __get_item_code(item_code,filter_text):
     
     if item_code and filter_text:
-        
-        count1 = Counter(eval(item_code))
-        
-        count2 = Counter(filter_text)
-
-        list3 = []
-        
-        for element in count1:
-            
-            if count1[element] == count2[element]:
-                
-                list3.append(element)
-
-        return list3
+                        
+        return [cod for cod in eval(item_code) if cod not in filter_text]
     
     return item_code
