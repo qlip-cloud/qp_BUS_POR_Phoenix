090d4a69a11c51c64048e6cb38b82f96e31175d3
Rafael Licett <rafael.licett@mentum.group>
2024-11-21 18:47:16 +0000

    [eval] error to confirm test 1

--- qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
+++ qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
@@ -470,7 +470,7 @@ def __confirm_sales_order(order_json, sales_order):
 
     if order_json.get('action') == "confirm":
 
-        #__send_check_out_so(sales_order)
+        __send_check_out_so(sales_order)
 
         __set_auto_discount(sales_order)
         
@@ -625,7 +625,7 @@ def __send_sales_order(sales_order):
     
     res = send_sales_order(sales_order, vf_SaleOrderConfirmError)
 
-    __set_sales_order_response(sales_order, res.get("reference"), res.get("response"))
+    #__set_sales_order_response(sales_order, res.get("reference"), res.get("response"))
             
     frappe.log_error(message=res.get("body_data"), title="GP Send Confirm")
 
