e0beb458df41d58d8b872e457829c2a95de35a59
Rafael Licett <rafael.licett@mentum.group>
2024-12-09 22:45:28 +0000

    [ajust] enabled checkout items]

--- qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
+++ qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
@@ -469,7 +469,7 @@ def __confirm_sales_order(order_json, sales_order):
 
     if order_json.get('action') == "confirm":
 
-        #__send_check_out_so(sales_order)
+        __send_check_out_so(sales_order)
 
         __set_auto_discount(sales_order)
                 
