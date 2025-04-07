ab0f46eec9326e861741c064056be3cab9884683
Rafael Licett <rafael.licett@mentum.group>
2025-04-07 17:52:21 +0000

    [add] validate delivery date in export list

--- qp_phonix_front/www/order/index.py
+++ qp_phonix_front/www/order/index.py
@@ -5,7 +5,7 @@ from qp_phonix_front.qp_phonix_front.uses_cases.sales_order.sales_order import s
 from qp_phonix_front.qp_phonix_front.services.try_catch import handler as try_catch
 from frappe.utils.xlsxutils import make_xlsx
 from qp_phonix_front.qp_phonix_front.uses_cases.shipping_method.shipping_method_list import __get_customer
-
+from qp_phonix_front.qp_phonix_front.tasks.update_delivery import update_delivery_data
 from datetime import datetime
 
 def get_context(context):
@@ -37,34 +37,32 @@ def export():
 
     customer = __get_customer()
 
-    sql = """
-        SELECT
-            sales_order.name
-            ,sales_order.qp_phonix_reference
-            ,sales_order.delivery_date
-            ,item.item_code
-            ,item.item_name
-            ,item.net_amount
-            ,sales_order.qp_phoenix_order_customer as OrdenCliente
-        FROM
-            `tabSales Order` as sales_order
-        INNER JOIN
-            `tabSales Order Item` as item
-            on (item.parent = sales_order.name)
-        where
-            status = 'To Deliver and Bill' and
-            customer = '{}'
-            
-    """.format(customer.name)
+    rows = frappe.get_list("Sales Order", filters = {"status": "To Deliver and Bill", "customer": customer.name }, pluck = "name")
     
-    #rows = frappe.get_list("Sales Order", filters = {"status": "To Deliver and Bill", "customer": customer.name },                            fields = ["name", "qp_phonix_reference", "delivery_date", "items"],                            as_list = True)
-    
-    rows = frappe.db.sql(sql, as_list = True)
-    
-    content += list(map(lambda row: list(row),rows))
+    for name in rows:
+        
+        order = frappe.get_doc("Sales Order", name)
+        
+        update_delivery_data(order)
+       
+        for item in order.items:
+        
+            content.append(get_line(order, item))
+        
+    #content += list(map(lambda row: list(row),rows))
    
     xlsx_file = make_xlsx(content, filename)
     # write out response as a xlsx type
     frappe.response['filename'] = filename + '.xlsx'
     frappe.response['filecontent'] = xlsx_file.getvalue()
     frappe.response['type'] = 'binary'
+    
+def get_line(order, item):
+    
+   return [
+        order.name,
+        order.qp_phonix_reference,
+        item.delivery_date,
+        item.item_code,
+        item.item_name,
+        item.net_amount]
