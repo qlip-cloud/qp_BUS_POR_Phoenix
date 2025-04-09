f8ed93df54a717002d9e1b886bc2f3ba6a9d9e2e
Rafael Licett <rafael.licett@mentum.group>
2025-04-09 20:32:45 +0000

    [test] document edit except validation modificate item create v4

--- qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
+++ qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
@@ -468,10 +468,12 @@ def __confirm_sales_order(order_json, sales_order):
         __send_check_out_so(sales_order)
 
         __set_auto_discount(sales_order)
-                
+        sales_order.reload()
         __send_sales_order(sales_order)
+        sales_order.reload()
         
         set_qp_subtotal(sales_order)
+        sales_order.reload()
                 
         sales_order.submit()
         
@@ -555,7 +557,7 @@ def __set_auto_discount(sales_order):
                             
         __save_coupon(coupon_control)
     
-        sales_order.save()
+    sales_order.save()
              
 def __update_order_items(sales_order, item):
     
