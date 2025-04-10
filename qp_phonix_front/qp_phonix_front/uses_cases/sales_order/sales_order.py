9bf5d212cdafa1a32d666c81e31dc8fce94d5e73
Rafael Licett <rafael.licett@mentum.group>
2025-04-10 17:04:05 +0000

    [finish] test confirm modificate error

--- qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
+++ qp_phonix_front/qp_phonix_front/uses_cases/sales_order/sales_order.py
@@ -465,8 +465,6 @@ def __confirm_sales_order(order_json, sales_order):
 
     if order_json.get('action') == "confirm":
         
-        sales_order.save()
-
         __send_check_out_so(sales_order)
 
         __set_auto_discount(sales_order)
@@ -556,8 +554,9 @@ def __set_auto_discount(sales_order):
                 __update_order_items(sales_order, item)        
                             
         __save_coupon(coupon_control)
+
     
-    sales_order.save()
+        sales_order.save()
              
 def __update_order_items(sales_order, item):
     
