77a4ad2bd6e6bdd389852b309b6b7099c36213ed
Rafael Licett <rafael.licett@mentum.group>
2025-04-10 18:57:29 +0000

    [add] delivery date update export

--- qp_phonix_front/qp_phonix_front/uses_cases/export/order_xlsx.py
+++ qp_phonix_front/qp_phonix_front/uses_cases/export/order_xlsx.py
@@ -2,29 +2,30 @@ import frappe
 from frappe.utils.xlsxutils import make_xlsx
 from qp_phonix_front.qp_phonix_front.uses_cases.sales_order.sales_order import get_sales_order
 from gp_phonix_integration.gp_phonix_integration.use_case.get_item_inventary import get_item_order as get_item_inventary
+from qp_phonix_front.qp_phonix_front.tasks.update_delivery import update_delivery_data
 
 @frappe.whitelist()
 def export_xlsx(order_id):
 
-    #xlsx_data, column_widths = build_xlsx_data(columns, data, visible_idx, include_indentation)
-    order = get_sales_order(order_id)
+    order = frappe.get_doc("Sales Order",order_id)
+    
+    update_delivery_data(order)
 
-    order.update({
-        "items" : get_item_inventary(order.get("items"))
-    })
+    order_dic = order.as_dict()
+    
+    items_update = get_item_inventary(order_dic.get("items"))
 
     items =list(map(lambda item: [
         item.item_name,
         item.item_code,
-        item.cantidad,
-        "Unidad x {}".format(int(item.uom_convertion[0].conversion_factor)),
-        item.price,
-        item.total,
+        item.qty,
+        "Unidad x {}".format(int(item.conversion_factor)),
+        item.net_rate,
+        item.net_amount,
+        item.delivery_date,
         "SI" if item.quantity > 0 else "NO"
         
-    ], order.get("items")))
-
-    #print(items)
+    ], items_update))
 
     xlsx_data = [
         [           
@@ -34,6 +35,7 @@ def export_xlsx(order_id):
             "Pedir por paquetes de",
             "Precio",
             "Total",
+            "Fecha de entrega",
             "Disponibilidad"
 
         ],
@@ -41,6 +43,7 @@ def export_xlsx(order_id):
 
 
     ]
+    
     xlsx_file = make_xlsx(xlsx_data, order_id)
 
     frappe.response["filename"] = "orden_"+ order_id + ".xlsx"
