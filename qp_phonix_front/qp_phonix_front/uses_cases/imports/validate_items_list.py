51a5263bfafefae49bdf2007378dc61515b9d5e6
Ashley <ashleydefort@outlook.es>
2025-05-02 15:33:18 +0000

    refactor(validation): replace validation logic with reusable function for item validation

--- qp_phonix_front/qp_phonix_front/uses_cases/imports/validate_items_list.py
+++ qp_phonix_front/qp_phonix_front/uses_cases/imports/validate_items_list.py
@@ -1,14 +1,14 @@
 import frappe
 from frappe import _
-from ..item_list.item_list import paginator_item_list as get_item_list
+from ..item_list.item_list import get_item_list 
 
 def validate_items_for_customer(items_list, idlevel):
     """
     Valida que cada item esté permitido para el idlevel del usuario
     y retorna su información completa si es válido.
     """
-    items_code_list = [item.get("item_code") for item in items_list]
-    valid_items = get_item_list(item_code_list=items_code_list, has_limit=False, idlevel=idlevel)
+    item_code_list = [item.get("item_code") for item in items_list]
+    valid_items = get_item_list(item_code_list=item_code_list, idlevel=idlevel)
     valid_item_codes = {item.item_code for item in valid_items}
 
     enriched = []
