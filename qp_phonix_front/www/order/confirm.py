3bbedb635ef0e4db8a2153e16e29968f9891dac5
Ashley <ashleydefort@outlook.es>
2025-06-17 23:45:14 +0000

    fix(flete-config): fix name of sales order property

--- qp_phonix_front/www/order/confirm.py
+++ qp_phonix_front/www/order/confirm.py
@@ -177,7 +177,7 @@ def set_flete_config(context, order):
 
     if flete_config:
         min_flete = flete_config[0].valor_minimo
-        qualifies_for_free_shipping = order.total_format >= min_flete
+        qualifies_for_free_shipping = order.net_total >= min_flete
         context.min_flete = min_flete
         context.qualifies_for_free_shipping = qualifies_for_free_shipping
         
